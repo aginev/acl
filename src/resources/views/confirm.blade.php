f25ed4d680d1aa2e9a8be76dddb352c600318c58
Atanas Ginev <aginev@1stonlineitteam.co.uk>
2015-06-04 17:07:58 +0300

    Confirm dialog moved to separate view

---
+++ src/resources/views/confirm.blade.php
@@ -0,0 +1,21 @@
+<!-- BEGIN DELETE MODAL -->
+<div class="modal bs-modal-sm" id="modal-confirm" tabindex="-1" role="dialog" aria-hidden="true">
+	<div class="modal-dialog modal-sm">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+				<h4 class="modal-title">Confirm Your Action</h4>
+			</div>
+			<div class="modal-body">
+				Are you sure?
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-success" data-dismiss="modal">No</button>
+				<button type="button" class="btn btn-danger" data-confirm="yes">Yes</button>
+			</div>
+		</div>
+		<!-- /.modal-content -->
+	</div>
+	<!-- /.modal-dialog -->
+</div>
+<!-- END DELETE MODAL -->
