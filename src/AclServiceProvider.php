dada298f38cf23d5f629b7e7afd88d348a840bbc
Atanas Ginev <aginev@users.noreply.github.com>
2015-06-17 11:06:18 +0300

    Vendor/Package were with upper case

--- src/AclServiceProvider.php
+++ src/AclServiceProvider.php
@@ -17,39 +17,39 @@ class AclServiceProvider extends ServiceProvider
     public function boot(Router $router)
     {
         // Tell Laravel where the views for a given namespace are located.
-        $this->loadViewsFrom(base_path('/vendor/Aginev/Acl/src/resources/views'), 'acl');
+        $this->loadViewsFrom(base_path('vendor/aginev/acl/src/resources/views'), 'acl');
 
         $this->publishes([
-            base_path('/vendor/Aginev/Acl/src/resources/views') => base_path('resources/views/vendor/acl'),
+            base_path('vendor/aginev/acl/src/resources/views') => base_path('resources/views/vendor/acl'),
         ], 'views');
 
         // Publish assets
         $this->publishes([
-            base_path('/vendor/Aginev/Acl/src/public') => public_path('vendor/acl'),
+            base_path('vendor/aginev/acl/src/public') => public_path('vendor/acl'),
         ], 'public');
 
         // Tell Laravel where the translations for a given namespace are located.
-        $this->loadTranslationsFrom(base_path('/vendor/Aginev/Acl/src/resources/lang'), 'acl');
+        $this->loadTranslationsFrom(base_path('vendor/aginev/acl/src/resources/lang'), 'acl');
 
         $this->publishes([
-            base_path('/vendor/Aginev/Acl/src/resources/lang') => base_path('resources/lang/vendor/acl'),
+            base_path('vendor/aginev/acl/src/resources/lang') => base_path('resources/lang/vendor/acl'),
         ], 'lang');
 
         // Merge config
-        $this->mergeConfigFrom(base_path('/vendor/Aginev/Acl/src/config/acl.php'), 'acl');
+        $this->mergeConfigFrom(base_path('vendor/aginev/acl/src/config/acl.php'), 'acl');
 
         $this->publishes([
-            base_path('/vendor/Aginev/Acl/src/config/acl.php') => config_path('acl.php'),
+            base_path('vendor/aginev/acl/src/config/acl.php') => config_path('acl.php'),
         ], 'config');
 
         // Publish migrations
         $this->publishes([
-            base_path('/vendor/Aginev/Acl/src/Database/Migrations/') => base_path('/database/migrations')
+            base_path('vendor/aginev/acl/src/Database/Migrations/') => base_path('/database/migrations')
         ], 'migrations');
 
         // Publish seeds
         $this->publishes([
-            base_path('/vendor/Aginev/Acl/src/Database/Seeds/') => base_path('/database/seeds')
+            base_path('vendor/aginev/acl/src/Database/Seeds/') => base_path('/database/seeds')
         ], 'seeds');
 
         // Define the ACL route middleware
@@ -59,7 +59,7 @@ class AclServiceProvider extends ServiceProvider
          * Including A Routes File From A Service Provider
          * NB! Keep this line at the very end of the method to be able to use the config at routes.php
          */
-        include base_path('/vendor/Aginev/Acl/src/Http/routes.php');
+        include base_path('vendor/aginev/acl/src/Http/routes.php');
     }
 
     /**
